f53e166677d86884ad2986b2858264649472a313
tmoka <knowledzr+alt@gmail.com>
2022-08-04 14:05:25 +0900

    スタート地点とゴール地点を取得する処理を実装

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
-import { BoardType } from './types';
+import { BoardType, PointType } from './types';
 
 const filePath = '../problems/1/problem1.txt';
 
@@ -8,7 +8,27 @@ fs.readFile(filePath, 'utf-8', (err, data) => {
     console.error('ファイルが読み込めません: ', err);
   }
   const board = data.split('\n').map((array) => array.split('')) as BoardType;
-  solver();
+  solver(board);
 });
 
-const solver = () => { };
+const solver = (board: BoardType) => {
+  getStartAndGoal(board);
+};
+
+const getStartAndGoal = (board: BoardType): { startPoint: PointType; goalPoint: PointType } => {
+  let startPoint: PointType = { x: 0, y: 0 };
+  let goalPoint: PointType = { x: 0, y: 0 };
+  board.map((elems, i) =>
+    elems.map((elem, j) => {
+      elem === 'S' ? (startPoint = { x: j, y: i }) : null;
+      elem === 'G' ? (goalPoint = { x: j, y: i }) : null;
+    }),
+  );
+  if (startPoint.x === 0 && startPoint.y === 0) {
+    console.error('入力データにスタート地点がありません');
+  }
+  if (goalPoint.x === 0 && goalPoint.y === 0) {
+    console.error('入力データにゴール地点がありません');
+  }
+  return { startPoint, goalPoint };
+};
